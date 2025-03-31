04b34590570f456e820853bdb4dff73b1267f64b
Fraser Buchanan <135606706+Fraser-Buchanan@users.noreply.github.com>
2025-03-31 15:42:22 +0100

    added write through brickster

--- Shiny/Read_DB_API/app.R
+++ Shiny/Read_DB_API/app.R
@@ -25,6 +25,21 @@ x    <- penguins$body_mass_g
 print(x)
 
 print(penguins)
+
+gentoo <- subset(penguins, species == "Gentoo")
+print(gentoo)
+
+gentoo <- subset(penguins, species == "Gentoo")
+
+write.csv(gentoo,"gentoo.csv", row.names = FALSE)
+
+db_volume_write(
+  '/Volumes/prd_dash_lab/dash_data_science_unrestricted/shared_external_volume/gentoo.csv',
+  'gentoo.csv',
+  overwrite = FALSE,
+  perform_request = TRUE
+)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
